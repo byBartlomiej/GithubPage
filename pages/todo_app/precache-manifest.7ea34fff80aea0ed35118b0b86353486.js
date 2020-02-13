6eaf0b52d5e31e474309a486798f86a98e11ae64
Bartlomiej <bartlomiejbartosiewicz@wp.pl>
2020-02-13 10:14:08 +0100

    change style,js - todp_app

---
+++ pages/todo_app/precache-manifest.7ea34fff80aea0ed35118b0b86353486.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f337654deb850a9c132f460a8ea31c7b",
+    "url": "/GithubPage/pages/todo_app/index.html"
+  },
+  {
+    "revision": "574e5868c58f8173209d",
+    "url": "/GithubPage/pages/todo_app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d659ca271117bdc0ae50",
+    "url": "/GithubPage/pages/todo_app/static/css/main.36b9e98a.chunk.css"
+  },
+  {
+    "revision": "574e5868c58f8173209d",
+    "url": "/GithubPage/pages/todo_app/static/js/2.ddfb4899.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/GithubPage/pages/todo_app/static/js/2.ddfb4899.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d659ca271117bdc0ae50",
+    "url": "/GithubPage/pages/todo_app/static/js/main.d6bcd0f8.chunk.js"
+  },
+  {
+    "revision": "6e2e1b4b6c38b91ff9ee",
+    "url": "/GithubPage/pages/todo_app/static/js/runtime-main.39a98378.js"
+  },
+  {
+    "revision": "22f23520abd17be73b7969bb7bb0e1e0",
+    "url": "/GithubPage/pages/todo_app/static/media/bg.22f23520.png"
+  }
+]);
