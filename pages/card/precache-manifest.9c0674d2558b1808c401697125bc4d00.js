67563a56c251ec5d1d3f74dec9438a33d9504e63
Bartlomiej <bartlomiejbartosiewicz@wp.pl>
2020-02-18 19:47:30 +0100

    add new page/style/js - card.html

---
+++ pages/card/precache-manifest.9c0674d2558b1808c401697125bc4d00.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39d8a9140264d127c23b399a981484b8",
+    "url": "/GithubPage/pages/card/index.html"
+  },
+  {
+    "revision": "51c945016de9a01f99b0",
+    "url": "/GithubPage/pages/card/static/css/main.eeff5742.chunk.css"
+  },
+  {
+    "revision": "7ee7d66ca9953a44a264",
+    "url": "/GithubPage/pages/card/static/js/2.b124cb4b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/GithubPage/pages/card/static/js/2.b124cb4b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "51c945016de9a01f99b0",
+    "url": "/GithubPage/pages/card/static/js/main.b1107b0d.chunk.js"
+  },
+  {
+    "revision": "edb91f3b665ef3f1be10",
+    "url": "/GithubPage/pages/card/static/js/runtime-main.b3027cd9.js"
+  },
+  {
+    "revision": "ef8814f8bcbe9d24792a5b576e08c295",
+    "url": "/GithubPage/pages/card/static/media/bgi.ef8814f8.jpg"
+  },
+  {
+    "revision": "8f720dcd1ec9508764250f5ccdb09153",
+    "url": "/GithubPage/pages/card/static/media/biuro.8f720dcd.jpg"
+  },
+  {
+    "revision": "db95bfd055d437df8b7b9e0dcd36e882",
+    "url": "/GithubPage/pages/card/static/media/logo.db95bfd0.png"
+  },
+  {
+    "revision": "ee7949b63f2b1048f117d745669590a9",
+    "url": "/GithubPage/pages/card/static/media/okno.ee7949b6.jpg"
+  },
+  {
+    "revision": "2671ca21ea273d71c24a9c46c1acb2f8",
+    "url": "/GithubPage/pages/card/static/media/remonty.2671ca21.jpg"
+  },
+  {
+    "revision": "b3f91ee5144ac261dd43e3fffb48f0da",
+    "url": "/GithubPage/pages/card/static/media/szyba1.b3f91ee5.jpg"
+  },
+  {
+    "revision": "7ebd3da7647573cb915c1f0ae26070d6",
+    "url": "/GithubPage/pages/card/static/media/szyba2.7ebd3da7.png"
+  }
+]);
